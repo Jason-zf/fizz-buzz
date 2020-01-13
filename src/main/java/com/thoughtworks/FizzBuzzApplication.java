a7a3f6ccf48264d35df0b16337580d727e2df202
baofengZheng <bfzheng@thoughtworks.com>
2020-01-13 22:32:03 +0800

    [baofeng] add pom and README

---
+++ src/main/java/com/thoughtworks/FizzBuzzApplication.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+
+public class FizzBuzzApplication {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 120; i++) {
+            String result = fizzBuzz(i + 1);
+            System.out.println(result);
+        }
+    }
+
+    private static String fizzBuzz(int digit) {
+        return null;
+    }
+}
